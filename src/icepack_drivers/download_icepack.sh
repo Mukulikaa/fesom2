7e8d635f65a13f6c782957fb43bde74932a0f79c
Mukulika Pahari <mukulikapahari@gmail.com>
2026-06-10 10:12:30 +0200

    get latest icepack

--- src/icepack_drivers/download_icepack.sh
+++ src/icepack_drivers/download_icepack.sh
@@ -1,11 +1,10 @@
 # Download icepack columphysics code
-# from Lorenzo's fork on github and
+# from CICE-Consortium on github and
 # switch to the appropriate branch
 
 DIR="./Icepack"
 if [ ! -d "$DIR" ]; then
-   git clone https://github.com/lzampier/Icepack.git
-   cd $DIR
-   # git checkout icepack_fesom2
-   git checkout icepack_fesom2_bugfix
+    git clone https://github.com/CICE-Consortium/Icepack.git
+    cd $DIR
+    git checkout Icepack1.5.3
 fi
