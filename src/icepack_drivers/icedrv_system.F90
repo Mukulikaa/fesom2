7dcd95a5d81ea13ea293e1e8d5c64a17c3682306
Mukulika Pahari <mukulikapahari@gmail.com>
2026-06-12 11:19:07 +0200

    copy over frank's changes

--- src/icepack_drivers/icedrv_system.F90
+++ src/icepack_drivers/icedrv_system.F90
@@ -1,71 +1,95 @@
 !=======================================================================
-!
+
 ! Diagnostic information output during run
 !
 ! author: Tony Craig
 
-      module icedrv_system
+module icedrv_system
 
-      use icedrv_kinds
-      use mod_parsup,       only: par_ex
-      use icedrv_constants, only: ice_stderr
-      use icepack_intfc,    only: icepack_warnings_flush, icepack_warnings_aborted
-      use mod_partit
-      implicit none
+  use icedrv_kinds
+  use icedrv_constants, only: ice_stderr
+  ! use icedrv_main, only: aice ! does not work because icedrv_system is not defined as a submodule - frank.kauker@awi.de
+  use icepack_intfc, only: icepack_warnings_flush, icepack_warnings_aborted
 
-      public :: icedrv_system_abort, icedrv_system_init
-   
-      private
-      type(t_partit), save, pointer  :: p_partit             ! a pointer to the mesh partitioning (has been accessed via "use g_parsup" in the original code)
-!=======================================================================
+  ! FESOM2 modules
+  use mod_parsup, only: par_ex
+  use mod_partit
+  
+  implicit none
+  
+  public :: icedrv_system_abort, icedrv_system_init, icedrv_system_flush
+  
+  private
+  type(t_partit), save, pointer :: p_partit ! a pointer to the mesh partitioning (has been accessed via "use g_parsup" in the original code)
 
-      contains
-
-!=======================================================================
-
-      subroutine icedrv_system_abort(icell, istep, string, file, line)
-
-      integer (kind=int_kind), intent(in), optional :: &
+  !=======================================================================
+  
+contains
+  
+  !=======================================================================
+  ! prints error information prior to aborting
+  
+  subroutine icedrv_system_abort(icell, istep, string, file, line)
+    
+    integer (kind=int_kind), intent(in), optional :: &
          icell       , & ! indices of grid cell where model aborts
          istep       , & ! time step number
          line            ! line number
+    
+    character (len=*), intent(in), optional :: string, file
+    
+    ! local variables
+    
+    character(len=*), parameter :: subname='(icedrv_system_abort)'
+    
+    write(ice_stderr,*) ' '
+    
+    call icepack_warnings_flush(ice_stderr)
+    
+    write(ice_stderr,*) ' '
+    write(ice_stderr,*) subname,' ABORTED: '
+    if (present(file))   write (ice_stderr,*) subname,' called from ', trim(file)
+    if (present(line))   write (ice_stderr,*) subname,' line number',  line
+    if (present(istep))  write (ice_stderr,*) subname,' istep =',      istep
+    ! if (present(icell))  write (ice_stderr,*) subname,' i, aice =',    icell, aice(icell) - see above
+    if (present(string)) write (ice_stderr,*) subname,' string =',     trim(string)
+    
+    ! Stop FESOM2
+    
+    call par_ex(p_partit%MPI_COMM_FESOM, p_partit%mype, 1)
+    
+    stop
 
-      character (len=*), intent(in), optional :: string, file
-
-      ! local variables
-
-      character(len=*), parameter :: subname='(icedrv_system_abort)'
-
-      write(ice_stderr,*) ' '
-
-      call icepack_warnings_flush(ice_stderr)
-
-      write(ice_stderr,*) ' '
-      write(ice_stderr,*) subname,' ABORTED: '
-      if (present(file))   write (ice_stderr,*) subname,' called from ', trim(file)
-      if (present(line))   write (ice_stderr,*) subname,' line number',  line
-      if (present(istep))  write (ice_stderr,*) subname,' istep =',      istep
-      if (present(string)) write (ice_stderr,*) subname,' string =',     trim(string)
-       
-      ! Stop FESOM2
-
-      call par_ex(p_partit%MPI_COMM_FESOM, p_partit%mype, 1)
-      stop
-
-      end subroutine icedrv_system_abort
+  end subroutine icedrv_system_abort
+    
+  !=======================================================================
+  ! flushes iunit IO buffer
+  
+  subroutine icedrv_system_flush(iunit)
+    
+    integer (kind=int_kind), intent(in) :: &
+         iunit        ! unit number to flush
+    
+    ! local variables
+    
+    character(len=*), parameter :: subname='(icedrv_system_flush)'
+    
+#ifndef NO_F2003
+    flush(iunit)
+#endif   
+    
+  end subroutine icedrv_system_flush
+  
+  !=======================================================================
+  subroutine icedrv_system_init(partit)
+    implicit none
+    type(t_partit), intent(inout), target :: partit
+    
+    p_partit => partit
+  end subroutine icedrv_system_init
+  
+  !=======================================================================
+  
+end module icedrv_system
 
 !=======================================================================
-      subroutine icedrv_system_init(partit)
-      implicit none
-      type(t_partit), intent(inout), target :: partit
-
-      p_partit => partit
-      end subroutine icedrv_system_init
-
-!=======================================================================
-
-
-      end module icedrv_system
-
-!=======================================================================
-
--- src/icepack_drivers/icedrv_transfer.F90
+++ src/icepack_drivers/icedrv_transfer.F90
@@ -10,74 +10,68 @@
 
       contains
 
-      module subroutine fesom_to_icepack(ice, mesh)
+        module subroutine fesom_to_icepack(ice, mesh)
 
-          use g_forcing_arrays, only: Tair, shum, u_wind,   v_wind,       & ! Atmospheric forcing fields
-                                      shortwave,  longwave, prec_rain,    &
-                                      prec_snow,  press_air
-          use g_forcing_param,  only: ncar_bulk_z_wind, ncar_bulk_z_tair, &
-                                      ncar_bulk_z_shum
-          use g_sbf,            only: l_mslp                     
-          use icepack_intfc,    only: icepack_warnings_flush, icepack_warnings_aborted
-          use icepack_intfc,    only: icepack_query_parameters
-          use icepack_intfc,    only: icepack_sea_freezing_temperature
-          use g_comm_auto,      only: exchange_nod
-          use icedrv_system,    only: icedrv_system_abort
-          use g_config,         only: dt
-          use o_param,          only: mstep
+          ! fesom modules
+          use g_forcing_arrays, only: Tair, shum, u_wind, v_wind,       & ! Atmospheric forcing fields
+               shortwave, longwave, prec_rain, prec_snow, press_air
+          use g_sbf, only: l_mslp                     
+          use icepack_intfc, only: icepack_warnings_flush, icepack_warnings_aborted, &
+               icepack_query_parameters, icepack_sea_freezing_temperature
+          use g_comm_auto, only: exchange_nod
+          use icedrv_system, only: icedrv_system_abort
           use mod_mesh
           use mod_tracer
           use mod_ice
           use g_clock
+          ! icepack modules
+          use icepack_intfc, only: icepack_warnings_flush, icepack_warnings_aborted, &
+	       icepack_query_parameters, icepack_sea_freezing_temperature
+          use icedrv_system, only: icedrv_system_abort
  
           implicit none
 
           character(len=*), parameter :: subname='(fesom_to_icepack)'
 
-          logical (kind=log_kind) :: &
-             calc_strair
-    
+          logical(kind=log_kind) :: calc_strair
+          integer(kind=int_kind) :: i, n,  k,  elem
+          real(kind=dbl_kind)    :: tx, ty, tvol, rhowat, aux, cprho
+
+          type(t_mesh), target, intent(in)     :: mesh
+          type(t_ice), target, intent(inout)   :: ice
+          real(kind=WP), dimension(:), pointer :: u_ice, v_ice, S_oc_array, T_oc_array, & 
+               u_w, v_w, stress_atmice_x, stress_atmice_y
+
+          ! fesom parameter
           real (kind=dbl_kind), parameter :: &
-             frcvdr = 0.28_dbl_kind,    & ! frac of incoming sw in vis direct band
-             frcvdf = 0.24_dbl_kind,    & ! frac of incoming sw in vis diffuse band
-             frcidr = 0.31_dbl_kind,    & ! frac of incoming sw in near IR direct band
-             frcidf = 0.17_dbl_kind,    & ! frac of incoming sw in near IR diffuse band
-             R_dry  = 287.05_dbl_kind,  & ! specific gas constant for dry air (J/K/kg)
-             R_vap  = 461.495_dbl_kind, & ! specific gas constant for water vapo (J/K/kg)
-             ex     = 0.286_dbl_kind
-             !PS rhowat = 1025.0_dbl_kind,  & ! Water density
-             !PS cc     = rhowat*4190.0_dbl_kind, & ! Volumetr. heat cap. of water [J/m**3/K](cc = rhowat*cp_water)
-             
-          integer(kind=int_kind)   :: i, n,  k,  elem
-          real   (kind=dbl_kind)   :: tx, ty, tvol
-          real   (kind=dbl_kind)   :: rhowat
+               frcvdr = 0.28_dbl_kind,    & ! frac of incoming sw in vis direct band
+               frcvdf = 0.24_dbl_kind,    & ! frac of incoming sw in vis diffuse band
+               frcidr = 0.31_dbl_kind,    & ! frac of incoming sw in near IR direct band
+               frcidf = 0.17_dbl_kind,    & ! frac of incoming sw in near IR diffuse band
+               R_dry  = 287.05_dbl_kind,  & ! specific gas constant for dry air (J/K/kg)
+               R_vap  = 461.495_dbl_kind, & ! specific gas constant for water vapo (J/K/kg)
+               ex     = 0.286_dbl_kind
+          ! PS rhowat = 1025.0_dbl_kind,  & ! Water density
+          ! PS cc     = rhowat*4190.0_dbl_kind, & ! Volumetr. heat cap. of water [J/m**3/K](cc = rhowat*cp_water)
 
-          real (kind=dbl_kind) :: &
-             aux,                 &
-             cprho
-
-          type(t_mesh), target, intent(in) :: mesh
-          type(t_ice), target, intent(inout) :: ice
-          real(kind=WP), dimension(:), pointer  :: u_ice, v_ice, S_oc_array, T_oc_array, & 
-                                                   u_w, v_w, stress_atmice_x, stress_atmice_y
 #include "associate_mesh.h"
-          
+
           ! make sure we use the same water density to scale precip and snow in 
           ! fesom and icepack
           call icepack_query_parameters(rhow_out=rhowat) 
 
-          u_ice      => ice%uice(:)
-          v_ice      => ice%vice(:)
-          S_oc_array => ice%srfoce_salt(:)
-          T_oc_array => ice%srfoce_temp(:)
-          u_w        => ice%srfoce_u(:)
-          v_w        => ice%srfoce_v(:)
+          u_ice           => ice%uice(:)
+          v_ice           => ice%vice(:)
+          S_oc_array      => ice%srfoce_salt(:)
+          T_oc_array      => ice%srfoce_temp(:)
+          u_w             => ice%srfoce_u(:)
+          v_w             => ice%srfoce_v(:)
           stress_atmice_x => ice%stress_atmice_x(:)
           stress_atmice_y => ice%stress_atmice_y(:)
           ! Ice 
     
-          uvel(:)  = u_ice(:)
-          vvel(:)  = v_ice(:)
+          uvel(:)   = u_ice(:)
+          vvel(:)   = v_ice(:)
     
           ! Atmosphere 
     
@@ -89,12 +83,7 @@
           flw(:)    = longwave(:)
           frain(:)  = prec_rain(:) * rhowat
           fsnow(:)  = prec_snow(:) * rhowat
-
           wind(:)   = sqrt(uatm(:)**2 + vatm(:)**2)
-
-          zlvl_t    = ncar_bulk_z_tair
-          zlvl_q    = ncar_bulk_z_shum
-          zlvl_v    = ncar_bulk_z_wind
 
           if ( l_mslp ) then
              potT(:) = T_air(:)*(press_air(:)/100000.0_dbl_kind)**ex             
@@ -107,17 +96,17 @@
     
           ! Ocean 
     
-          sst(:)    = T_oc_array(:)
-          sstdat(:) = T_oc_array(:)
-          sss(:)    = S_oc_array(:)
-          uocn(:)   = u_w(:)
-          vocn(:)   = v_w(:)
+          sst(:)     = T_oc_array(:)
+          sstdat(:)  = T_oc_array(:)
+          sss(:)     = S_oc_array(:)
+          uocn(:)    = u_w(:)
+          vocn(:)    = v_w(:)
 
           ! divide shortwave into spectral bands
-          swvdr = fsw*frcvdr        ! visible direct
-          swvdf = fsw*frcvdf        ! visible diffuse
-          swidr = fsw*frcidr        ! near IR direct
-          swidf = fsw*frcidf        ! near IR diffuse
+          swvdr      = fsw*frcvdr        ! visible direct
+          swvdf      = fsw*frcvdf        ! visible diffuse
+          swidr      = fsw*frcidr        ! near IR direct
+          swidf      = fsw*frcidf        ! near IR diffuse
 
           call icepack_query_parameters(calc_strair_out=calc_strair, cprho_out=cprho)
           call icepack_warnings_flush(ice_stderr)
@@ -125,99 +114,139 @@
               file=__FILE__,line= __LINE__)
     
           if (.not. calc_strair) then
-                strax(:) = stress_atmice_x(:)
-                stray(:) = stress_atmice_y(:)
+             strax(:) = stress_atmice_x(:)
+             stray(:) = stress_atmice_y(:)
           endif
     
           do i = 1, nx
-              ! ocean - ice stress
-              aux = sqrt((uvel(i)-uocn(i))**2+(vvel(i)-vocn(i))**2)*rhowat*ice%cd_oce_ice
-              strocnxT(i) = aux*(uvel(i) - uocn(i))
-              strocnyT(i) = aux*(vvel(i) - vocn(i))
-              ! freezing - melting potential
-              Tf(i)   = icepack_sea_freezing_temperature(sss(i))
-              frzmlt(i) = min((Tf(i)-sst(i)) * cprho * hmix(i) / dt, 1000.0_dbl_kind)
-           enddo
+             ! ocean - ice stress
+             aux = sqrt( (uvel(i)-uocn(i))**2 + (vvel(i)-vocn(i))**2 ) * rhowat * ice%cd_oce_ice
+             strocnxT(i) = aux * (uvel(i) - uocn(i))
+             strocnyT(i) = aux * (vvel(i) - vocn(i))
+             ! freezing - melting potential
+             Tf(i) = icepack_sea_freezing_temperature(sss(i))
+             ! depending on 'tfrz_option', 'Tf' (deg C) is:
+             ! 'mushy':       Tf = icepack_liquidus_temperature(sss)
+             ! 'linear_salt': Tf = -depressT * sss
+             ! 'constant':    Tf = Tocnfrz
+             ! 'minus1p8':    Tf = -1.8_dbl_kind
+             ! frank.kauker@awi.de
+             if(Tf(i) >=  c0) write(*,*) 'Tf >= 0: ', Tf(i), mype, geo_coord_nod2D(1, i), geo_coord_nod2D(2, i)
+             frzmlt(i) = min( (Tf(i)-sst(i)) * cprho * hmix(i) / dt, 1000.0_dbl_kind )
+             ! Tf < sst: frzmlt negative, i.e. ice can melt
+             ! Tf > sst: frzmlt positive, i.e. ice can grow
+             !if (geo_coord_nod2D(2, i) > 89.5 * 3.14159/180.) & ! calculate pole indices for analysis/debugging; frank.kauker@awi.de
+             !     write(*,*) 'pole index =', i, geo_coord_nod2D(1, i), geo_coord_nod2D(2, i), mype
+          enddo
+          !write(*,*) 'icedrv_transfer: min/max val(Tf)=', minval(Tf), maxval (Tf)
 
-           ! Compute convergence and shear on the nodes
+          ! Compute convergence and shear on the nodes
 
-           do i = 1, nx_nh
-              tvol = c0
-              tx   = c0
-              ty   = c0
-              do k = 1, nod_in_elem2D_num(i)
-                 elem = nod_in_elem2D(k,i)
-                 tvol = tvol + elem_area(elem)
-                 tx = tx + rdg_conv_elem(elem)  * elem_area(elem)
-                 ty = ty + rdg_shear_elem(elem) * elem_area(elem)
-              enddo
-              rdg_conv(i)  = tx / tvol
-              rdg_shear(i) = ty / tvol
-           enddo
-
-           call exchange_nod(rdg_conv, rdg_shear, p_partit)
+          do i = 1, nx_nh
+             tvol = c0
+             tx   = c0
+             ty   = c0
+             do k = 1, nod_in_elem2D_num(i)
+                elem = nod_in_elem2D(k,i)
+                tvol = tvol + elem_area(elem)
+                tx = tx + rdg_conv_elem(elem)  * elem_area(elem)
+                ty = ty + rdg_shear_elem(elem) * elem_area(elem)
+             enddo
+             rdg_conv(i)  = tx / tvol ! min(tx / tvol, 1.e-5)
+             rdg_shear(i) = ty / tvol ! min(ty / tvol, 1.e-5)
+          enddo
+          !write(*,*) 'icedrv_transfer: min/max rdg_conv = ', minval(rdg_conv), maxval(rdg_conv)
+          !write(*,*) 'icedrv_transfer: min/max rdg_shear = ', minval(rdg_shear), maxval(rdg_shear)
+          
+          call exchange_nod(rdg_conv, rdg_shear, p_partit, luse_g2g = .true.)
 
           ! Clock variables
     
           days_per_year = ndpyr
-          daymo         = num_day_in_month(fleapyear,:)
-          if (fleapyear==1) then
-             daycal     = daycal366
-          else
-             daycal     = daycal365
-          end if
-          istep1        = mstep
-          time          = mstep*dt
+          ! daymo         = num_day_in_month(fleapyear,:) - daymo is not used in Icepack columnphysics but only in the original drivers frank.kauker@awi.de
+          ! if (fleapyear==1) then - daycal is not used in Icepack columnphysics but only in the original drivers frank.kauker@awi.de
+          !   daycal     = daycal366
+          ! else
+          !    daycal     = daycal365
+          ! end if
+          ! istep1        = mstep - istep1 is not used in Icepack columnphysics but only in the original drivers frank.kauker@awi.de
+          ! time          = mstep*dt - time is not used in Icepack columnphysics but only in the original drivers frank.kauker@awi.de
           mday          = day_in_month
           month_i       = month
           nyr           = yearnew
-          sec           = timenew
-          yday          = real(ndpyr, kind=dbl_kind)
-          dayyr         = real(days_per_year, kind=dbl_kind)
-          secday        = real(sec, kind=dbl_kind)
-          calendar_type = 'Gregorian'
+          sec           = int(timenew)
+          yday          = real(daynew, kind=dbl_kind)
+          ! dayyr         = real(days_per_year, kind=dbl_kind) - dayyr is not used in Icepack columnphysics but only in the original drivers frank.kauker@awi.de
+          ! secday        = real(sec, kind=dbl_kind) - secday is defined in module icepack_parameters (icepack_parameters.F90) frank.kauker@awi.de
+          calendar_type = 'GREGORIAN'
           dt_dyn        = dt/real(ndtd,kind=dbl_kind) ! dynamics et al timestep
+          ! if (mype==0)  print *, 'fesom_to_icepack:', mday, sec, timenew, yday, calendar_type, dt_dyn
+        end subroutine fesom_to_icepack
 
-      end subroutine fesom_to_icepack
+        !=======================================================================
 
-!=======================================================================
+        module subroutine fesom_to_icepack_para()
 
+          ! fesom modules
+          use g_forcing_param, only: ncar_bulk_z_wind, ncar_bulk_z_tair, ncar_bulk_z_shum
+          use g_config, only: dt
+          use o_param, only: mstep
+          ! icepack modules
+          use icedrv_system, only: icedrv_system_abort
 
-      module subroutine icepack_to_fesom( nx_in,                           &
-                                          aice_out,  vice_out,  vsno_out,  &
-                                          fhocn_tot_out, fresh_tot_out,    &
-                                          strocnxT_out,  strocnyT_out,     &
-                                          dhs_dt_out,    dhi_dt_out,       &
-                                          fsalt_out,     evap_ocn_out,     &
-                                          evap_out)
+          character(len=*), parameter :: subname='(fesom_to_icepack_para)'
 
+          if ( ncar_bulk_z_tair .ne. ncar_bulk_z_shum ) then
+             call icedrv_system_abort(string=subname, file=__FILE__,line= __LINE__)
+          else
+             zlvl_s    = ncar_bulk_z_tair ! should be equal to ncar_bulk_z_shum
+             zlvl_v    = ncar_bulk_z_wind
+          endif
+
+        end subroutine fesom_to_icepack_para
+
+        !=======================================================================
+        
+        module subroutine icepack_to_fesom( &
+             nx_in, aice_out,  vice_out,  vsno_out,  &
+             fhocn_tot_out, fresh_tot_out,    &
+             strocnxT_out,  strocnyT_out,     &
+             dhs_dt_out,    dhi_dt_out,       &
+             fsalt_out,     evap_ocn_out,     &
+             evap_out)
+          
           implicit none
-
+          
           integer (kind=int_kind), intent(in) :: &
-             nx_in      ! block dimensions
-
+               nx_in      ! block dimensions
+          
           real (kind=dbl_kind), dimension(nx_in), intent(out), optional :: &
-             aice_out, &  
-             vice_out, &
-             vsno_out, &
-             fhocn_tot_out, &
-             fresh_tot_out, &
-             strocnxT_out,  &
-             strocnyT_out,  &
-             fsalt_out,     &
-             dhs_dt_out,    &
-             dhi_dt_out,    &
-             evap_ocn_out,  &
-             evap_out
-
+               aice_out, &  
+               vice_out, &
+               vsno_out, &
+               fhocn_tot_out, &
+               fresh_tot_out, &
+               strocnxT_out,  &
+               strocnyT_out,  &
+               fsalt_out,     &
+               dhs_dt_out,    &
+               dhi_dt_out,    &
+               evap_ocn_out,  &
+               evap_out
+          
           character(len=*),parameter :: subname='(icepack_to_fesom)'   
-
-
+          
           if (present(aice_out)              ) aice_out         = aice
           if (present(vice_out)              ) vice_out         = vice
           if (present(vsno_out)              ) vsno_out         = vsno
           if (present(fresh_tot_out)         ) fresh_tot_out    = fresh_tot
-          if (present(fhocn_tot_out)         ) fhocn_tot_out    = fhocn_tot
+          if (present(fhocn_tot_out)         ) then
+             where(sst > Tf)
+                                               fhocn_tot_out    = fhocn_tot
+             elsewhere
+                                               fhocn_tot_out    = c0
+             endwhere
+          endif
           if (present(strocnxT_out)          ) strocnxT_out     = strocnxT
           if (present(strocnyT_out)          ) strocnyT_out     = strocnyT
           if (present(dhi_dt_out)            ) dhi_dt_out       = dhi_dt
@@ -225,29 +254,24 @@
           if (present(fsalt_out)             ) fsalt_out        = fsalt
           if (present(evap_ocn_out)          ) evap_ocn_out     = evap_ocn
           if (present(evap_out)              ) evap_out         = evap
+          
+        end subroutine icepack_to_fesom
 
-      end subroutine icepack_to_fesom
+        !=======================================================================
 
-!=======================================================================
-
-      module subroutine icepack_to_fesom_single_point(nx_in,       &
-                                                      strength_out)
-
+        module subroutine icepack_to_fesom_single_point(nx_in, strength_out)
+          
           implicit none
 
-          integer (kind=int_kind), intent(in) :: &
-             nx_in      ! surface node or element
-
-          real (kind=dbl_kind), intent(out), optional :: &
-             strength_out
+          integer (kind=int_kind), intent(in) :: nx_in      ! surface node or element
+          real (kind=dbl_kind), intent(out), optional :: strength_out
 
           character(len=*),parameter :: subname='(icepack_to_fesom_single_point)'
 
-
           if (present(strength_out)          ) strength_out     = strength(nx_in)
 
-      end subroutine icepack_to_fesom_single_point
+        end subroutine icepack_to_fesom_single_point
 
-!=======================================================================
+        !=======================================================================
 
-      end submodule icedrv_transfer 
+      end submodule icedrv_transfer
